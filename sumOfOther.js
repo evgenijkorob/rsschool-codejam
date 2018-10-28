1d6938c31f9b0eaaa004d967365f6e2a03dd0236
evgenijkorob <evgenij.k.wie@gmail.com>
2018-10-28 15:49:02 +0300

    refactor: refactor brackets in sumOfOther()

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,7 +1,6 @@
 module.exports = function sumOfOther(array) {
-  if (array === undefined || array === null || !array.length) {
+  if (array === undefined || array === null || !array.length)
     return undefined;
-  }
   return array.map((element, index) => {
     let buf = array.filter((elem, i) => i !== index);
     return buf.reduce((sum, x) => sum + x);
